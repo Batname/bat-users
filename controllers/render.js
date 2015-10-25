178dd84f2f8c0f2cd38cedf9b3229fbf3ac9aa58
Batname <dadubinin@gmail.comm>
2015-10-25 21:05:42 +0200

    templete for google

--- controllers/render.js
+++ controllers/render.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const config = require('config');
+const path = require('path');
+const templatesPath = path.join(config.projectRoot, '/services/node_modules/bat-users/views');
+
 module.exports = {
     index(data, template) {
         return function* (){
             if ( this.state.isBot ) {
-                this.body = 'hello bot';
+                //this.body = 'hello bot';
+
+                yield this.render('index', {
+                  path: templatesPath,
+                  isBot: this.state.isBot,
+                  user: 'John'
+                });
             } else {
                 this.body = data;
             }
